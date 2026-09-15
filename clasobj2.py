b3ffc4386fc1e42d4b537c421590588e8d5cf3fc
Arockiya Ashika <arockiyaashika7701@gmail.com>
2026-09-15 21:08:30 +0530

    student name in class and object

---
+++ clasobj2.py
@@ -0,0 +1,9 @@
+class student:
+    def __init__(self,name,age,rollno):
+        self.name=name
+        self.age=age
+        self.rollno=rollno
+s1=student("ashika",22,2545)
+s2=student("nithya",23,3456)
+print("name:",s1.name,"\nage",s1.age,"\nrollno",s1.rollno)
+print("\nname:",s2.name,"\nage",s2.age,"\nrollno",s2.rollno)
